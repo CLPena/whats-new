20994bb51694e64a547cb45dcfe46c3ddf9bbabd
Cristina Pena <cristinalpena@gmail.com>
2020-03-17 18:19:19 -0600

    Adjust hover color

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,7 +5,7 @@ const Menu = () => {
     return (
       <nav className="menu">
         <h1 className="menu-header">WHAT'S NEW?</h1>
-        <button className="menu-button local-button">LOCAL NEWS</button>
+        <button className="menu-button local-button active-button">LOCAL NEWS</button>
         <button className="menu-button technology-button">TECHNOLOGY</button>
         <button className="menu-button entertainment-button">ENTERTAINMENT</button>
         <button className="menu-button science-button">SCIENCE</button>
